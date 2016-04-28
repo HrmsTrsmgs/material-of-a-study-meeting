bf2f4bc239c2a68884705de17ee608c0fdcf0fbe
新田光宏 <potimarimo@gmail.com>
2016-04-29 03:46:56 +0900

    コンパイル

---
+++ CssWriter/CssWriter/Helpers/ExtensionsForCssWriter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CssWriter.Helpers
+{
+    public static class ExtensionsForCssWriter
+    {
+        public static CssStatement Css(this string selector, params Func<string, string>[] decdeclarations)
+        {
+            return null;
+        }
+    }
+}
--- CssWriter/CssWriterTest/CssHelperTest.cs
+++ CssWriter/CssWriterTest/CssHelperTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using CssWriter.Helpers;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
