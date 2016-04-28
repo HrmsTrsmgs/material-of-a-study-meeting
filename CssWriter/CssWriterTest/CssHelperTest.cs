30ed1165ba864e875ad61315364f9a483407d67a
新田光宏 <potimarimo@gmail.com>
2016-04-29 03:42:11 +0900

    レッド　ヘルパーを使ってCssが組み立てられます

---
+++ CssWriter/CssWriterTest/CssHelperTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CssWriterTest
+{
+    [TestFixture]
+    public class CssHelperTest : AssertionHelper
+    {
+        [Test]
+        public void ヘルパーを使ってCssが組み立てられます()
+        {
+            var tested =
+                "H1".Css(
+                    font_size => "12pt",
+                    line_height => "10pt");
+
+            Expect(tested.ToString(), Is.EqualTo(
+@"H1 {
+    font-size: 12pt;
+    line-height: 10pt
+}"));
+        }
+    }
+}
