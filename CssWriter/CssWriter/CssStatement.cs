d83eb044ade0669736281de85a08ed634c873079
新田光宏 <potimarimo@gmail.com>
2016-04-29 01:39:41 +0900

    グリーン コンパイル

---
+++ CssWriter/CssWriter/CssStatement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CssWriter
+{
+    public class CssStatement
+    {
+        public Dictionary<string, string> Declarations { get; set; }
+        public string[] Selectors { get; set; }
+    }
+}
