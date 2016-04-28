fefd9eed0157d49e3a73e718964a0ceadfaa5d9b
新田光宏 <potimarimo@gmail.com>
2016-04-28 23:42:03 +0900

    レッド

---
+++ CssWriter/CssWriterTest/CssTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CssWriterTest
+{
+    [TestFixture]
+    public class CssTest : AssertionHelper
+    {
+
+
+        [Test]
+        public void ToStringで基本的なCSSの出力ができます()
+        {
+            var tested = new CssStatement();
+
+            tested.Selectors = new[] { "H1" };
+
+            tested.Declarations = new Dictionary<string, string> { { "font-size", "12pt" } };
+
+            Expect(tested.ToString(), Is.EqualTo(
+@"H1 {
+    font-size: 12pt
+}"));
+        }
+    }
+}
