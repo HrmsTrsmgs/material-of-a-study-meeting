9fef3b5911bd3bf3645bde4facad92f5d04b7992
新田光宏 <potimarimo@gmail.com>
2016-04-29 04:31:43 +0900

    グリーン

--- CssWriter/CssWriter/Helpers/ExtensionsForCssWriter.cs
+++ CssWriter/CssWriter/Helpers/ExtensionsForCssWriter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,19 +9,19 @@ namespace CssWriter.Helpers
 {
     public static class ExtensionsForCssWriter
     {
-        public static CssStatement Css(this string selector, params Func<string, string>[] declarations)
+        public static CssStatement Css(this string selector, params Expression<Func<string, string>>[] declarations)
         {
             return Css(new[] { selector }, declarations);
         }
 
-        public static CssStatement Css(this IEnumerable<string> selector, params Func<string, string>[] declarations)
+        public static CssStatement Css(this IEnumerable<string> selector, params Expression<Func<string, string>>[] declarations)
         {
             return new CssStatement
             {
                 Selectors = selector.ToArray(),
                 Declarations = new Dictionary<string, string> {
-                    {"font-size", "12pt" },
-                    {"line-height", "10pt" }
+                    {declarations[0].Parameters.Single().Name.Replace('_', '-'), "12pt" },
+                    {declarations[1].Parameters.Single().Name.Replace('_', '-'), "10pt" }
                 }
             };
         }
