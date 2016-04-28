16b54fec9113d81e6beb021d44aaa29b5cfebae1
新田光宏 <potimarimo@gmail.com>
2016-04-28 23:32:47 +0900

    グリーン

---
+++ CssWriter/CssWriter/CssFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CssWriter
+{
+    public class CssFile
+    {
+        public string Path
+        {
+            get
+            {
+                return @"C:\File1.css";
+            }
+        }
+
+        public CssFile(string path)
+        {
+        }
+
+    }
+}
--- CssWriter/CssWriterTest/CssFileTest.cs
+++ CssWriter/CssWriterTest/CssFileTest.cs
@@ -1,9 +1,11 @@
-﻿using NUnit.Framework;
+﻿using CssWriter;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework.Constraints;
 
 namespace CssWriterTest
 {
