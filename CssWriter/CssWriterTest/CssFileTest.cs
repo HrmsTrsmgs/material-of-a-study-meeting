20d15ee48851a4a1e5243c890d8ce7517d648ff5
新田光宏 <potimarimo@gmail.com>
2016-04-28 23:37:05 +0900

    レッド　三角測量

--- CssWriter/CssWriterTest/CssFileTest.cs
+++ CssWriter/CssWriterTest/CssFileTest.cs
@@ -18,6 +18,10 @@ namespace CssWriterTest
             var tested = new CssFile(@"C:\File1.css");
 
             Expect(tested.Path, Is.EqualTo(@"C:\File1.css"));
+
+            var tested2 = new CssFile(@"C:\File2.css");
+
+            Expect(tested2.Path, Is.EqualTo(@"C:\File2.css"));
         }
     }
 }
