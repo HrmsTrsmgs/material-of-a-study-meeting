255c48a3ba92e900af1101500cba800e30f46354
新田光宏 <potimarimo@gmail.com>
2016-04-29 03:52:06 +0900

    リファクタリング

--- CssWriter/CssWriter/Helpers/ExtensionsForCssWriter.cs
+++ CssWriter/CssWriter/Helpers/ExtensionsForCssWriter.cs
@@ -10,16 +10,14 @@ namespace CssWriter.Helpers
     {
         public static CssStatement Css(this string selector, params Func<string, string>[] decdeclarations)
         {
-            var ret = new CssStatement();
-
-            ret.Selectors = new[] { "H1" };
-
-            ret.Declarations = new Dictionary<string, string> {
-                {"font-size", "12pt" },
-                {"line-height", "10pt" }
+            return new CssStatement
+            {
+                Selectors = new[] { "H1" },
+                Declarations = new Dictionary<string, string> {
+                    {"font-size", "12pt" },
+                    {"line-height", "10pt" }
+                }
             };
-
-            return ret;
         }
     }
 }
