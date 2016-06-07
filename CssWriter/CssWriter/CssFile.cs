254718ef33d726a8ee234c9c41ddf7614d668af3
新田光宏 <potimarimo@gmail.com>
2016-06-07 23:51:57 +0900

    Revert "グリーン"
    This reverts commit 83a3ac1393674c6eabb23156c5dee36673b812cd.

--- CssWriter/CssWriter/CssFile.cs
+++ CssWriter/CssWriter/CssFile.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,7 +25,6 @@ namespace CssWriter
 
         public void Save()
         {
-            File.WriteAllText(Path, cssStatement.ToString());
         }
     }
 }
