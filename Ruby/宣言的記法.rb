fa8b2acab184ddaf9dd1e5fad0f58e8e21ec295a
新田光宏 <potimarimo@gmail.com>
2016-06-17 08:19:34 +0900

    Rubyの宣言的記法の追加

---
+++ Ruby/宣言的記法.rb
@@ -0,0 +1,60 @@
+class Something
+end
+
+class Example
+  attr_accessor :basic_price
+  attr_accessor :tax_free
+  attr_accessor :age
+
+  def calculate_sum(array)
+    sum = 0
+    array.each do |i|
+      sum += i
+    end
+
+    sum
+  end
+
+  def tax
+  	price * 0.08
+  end
+
+  def payment
+  	if tax_free
+  		price
+  	else
+  		price + tax
+  	end
+  end
+
+  def price
+  	case age
+  	when 0..10
+      basic_price * 0.3
+    when 11..19
+      basic_price * 0.8
+    when 20
+      basic_price * 0.1
+    else
+      basic_price
+    end
+  end
+
+
+
+  def output
+  	puts calculate_sum [1,3,5]
+
+  	self.basic_price = 1000
+  	self.age = 20
+  	puts price
+
+  	puts tax
+
+    puts payment
+  end
+end
+
+one ||= Something.new
+  
+Example.new.output
