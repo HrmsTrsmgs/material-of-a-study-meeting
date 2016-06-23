fb49f23365b1bd8d0ffe49253c989998151ad996
新田光宏 <potimarimo@gmail.com>
2016-06-24 01:17:45 +0900

    DynamicのList

---
+++ DynamicのList/ListExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicのList
+{
+    static class ListExtensions
+    {
+        public static void AddExpando(this IList<dynamic> self, int count)
+        {
+            for(int i = 0; i < count; ++i)
+            {
+                self.AddExpando();
+            }
+        }
+
+        public static void AddExpando(this IList<dynamic> self)
+        {
+            self.Add(new ExpandoObject());
+        }
+    }
+}
---
+++ DynamicのList/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicのList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<dynamic>();
+
+            list.Add("ABC");
+            list.Add(10);
+
+            Console.WriteLine(list[0] + "DEF");
+            Console.WriteLine(list[1] * 3);
+
+            list.AddExpando(3);
+
+            list[2].Name = "太郎";
+            list[2].Age = 18;
+
+            list[3].Name = "次郎";
+            list[3].Age = 17;
+
+            list[4].Name = "三郎";
+            list[4].Age = 16;
+
+            for (var i = 2; i <= 4; i++)
+            {
+                Console.WriteLine(list[i].Name);
+                Console.WriteLine(list[i].Age);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
