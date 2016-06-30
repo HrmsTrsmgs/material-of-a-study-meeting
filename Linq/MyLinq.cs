1947d7cc9bb6247a63f46e7de87805e3e6296b8d
新田光宏 <potimarimo@gmail.com>
2016-07-01 07:33:40 +0900

    Linqの追加

---
+++ Linq/MyLinq.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq.MyLinq
+{
+    static class EnumerableExtensions
+    {
+        public static IEnumerable<TResult> Select<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
+        {
+            foreach(var item in source)
+            {
+                yield return selector(item);
+            }
+        }
+
+        public static IEnumerable<TSource> Where<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            foreach(var item in source)
+            {
+                if(predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public static IOrderedEnumerable<TSource> OrderBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector) where TKey : IComparable<TKey>
+        {
+            return new OrderdEnumerable<TSource>(source).CreateOrderedEnumerable(keySelector, Comparer<TKey>.Create((key1, key2) => key1.CompareTo(key2)), false);
+        }
+
+        public static List<TSource> ToList<TSource>(this IEnumerable<TSource> source)
+        {
+            var list = new List<TSource>();
+
+            foreach(var item in source)
+            {
+                list.Add(item);
+            }
+            return list;
+        }
+
+        public class OrderdEnumerable<TElement> : IOrderedEnumerable<TElement>
+        {
+            public OrderdEnumerable(IEnumerable<TElement> source)
+            {
+                list = source.ToList();
+            }
+
+            List<TElement> list;
+
+            public IOrderedEnumerable<TElement> CreateOrderedEnumerable<TKey>(Func<TElement, TKey> keySelector, IComparer<TKey> comparer, bool descending)
+            {
+                list.Sort((item1, item2) => comparer.Compare(keySelector(item1), keySelector(item2)));
+
+                if (descending)
+                {
+                    list.Reverse();
+                }
+                return this;
+            }
+
+            public IEnumerator<TElement> GetEnumerator()
+            {
+                return list.GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+        }
+
+        public interface IOrderedEnumerable<TElement> : IEnumerable<TElement>, IEnumerable
+        {
+            IOrderedEnumerable<TElement> CreateOrderedEnumerable<TKey>(Func<TElement, TKey> keySelector, IComparer<TKey> comparer, bool descending);
+        }
+    }
+}
---
+++ Linq/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Linq;
+using Linq.MyLinq;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new[] { 3, 2, 7, 5, 1 };
+
+            //var result1 =
+            //    from i in data
+            //    select i * i;
+
+            //foreach (var item in result1)
+            //{
+            //}
+
+            //var result2 =
+            //    from i in data
+            //    where i < 6
+            //    select i * i;
+
+            //foreach (var item in result2)
+            //{
+            //}
+
+            var result3 =
+                from i in data
+                where i < 4
+                orderby i
+                select i * i;
+            foreach (var item in result3)
+            {
+            }
+        }
+    }
+}
