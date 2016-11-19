2026e8ce3879f7cfdb0a83b6393df931751c9573
NITTA Mitsuhiro <potimarimo@gmail.com>
2016-11-19 11:32:47 +0900

    アセンブリ言語を見てみる

--- アセンブリ言語見てみる/アセンブリ言語見てみる/アセンブリ言語見てみる.cpp
+++ アセンブリ言語見てみる/アセンブリ言語見てみる/アセンブリ言語見てみる.cpp
@@ -3,9 +3,63 @@
 
 #include "stdafx.h"
 
+int func1()
+{
+	return 0;
+}
+
+int func2()
+{
+	return 42;
+}
+
+void func3()
+{
+	int i = 3;
+	if (i < 3) {
+		i = 5;
+	}
+}
+
+void func4()
+{
+	int i = 3;
+	while (i < 5) {
+		i++;
+	}
+}
+void func5()
+{
+	for (int i = 0; i < 5; i++) {
+
+	}
+}
+
+void func6() 
+{
+	int i = 3;
+	switch (i) {
+	case 1:
+		i = 5;
+	case 2:
+		i = 7;
+	case 3:
+		i = 8;
+	default:
+		i = 10;
+	}
+}
+
+
 
 int main()
 {
+	func1();
+	func2();
+	func3();
+	func4();
+	func5();
+	func6();
     return 0;
 }
 
